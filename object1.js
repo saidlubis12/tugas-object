7b983c9a3109b3186661cf94ef2dcbba23f5c8a7
ninjaa <alkhudrisaid83@gmail.com>
2020-09-11 06:18:16 +0700

    first commit

---
+++ object1.js
@@ -0,0 +1,56 @@
+// var car = {
+
+//     name: 'avanza',
+//      tipe:  313332,
+    
+//     start : function(){
+//        document.write("Mobil berjalan")
+//      }
+//     }
+    
+//     car.start()
+
+
+
+// var nasi ={
+//   name:'goreng',
+
+// start : function(){
+//       document.write('nasi digoreng dadakan')
+// }
+// }
+// nasi.start()
+
+
+
+// function motor(nama, merek){
+//     var kendaraan={}
+//     kendaraan.namaMotor = nama
+//     kendaraan.merekMotor= merek
+
+//     console.log( kendaraan.namaMotor, kendaraan.merekMotor)
+
+// }
+// motor('kawasaki', 'ninjaa')
+
+
+
+// var siswa = {nama: 'said', usia:20}
+// var x = siswa
+
+// x.nama  = "Styrtyytry"
+// console.log(siswa)
+
+
+
+// function siswa(nama ,usia){
+//     var anak = {}
+//     anak.namaLengkap = nama,
+//     anak.usiaAnak = usia,
+
+//     console.log(anak.namaLengkap, anak.usiaAnak)
+// }
+// siswa('said', 13)
+
+
+
